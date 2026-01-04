c4e661cd725d0a77909330567597e2321584f2bb
RG <rgkg2@umsystem.edu>
2026-01-04 15:27:02 -0600

    Update generate_masks.py

--- utils/generate_masks.py
+++ utils/generate_masks.py
@@ -1,5 +1,9 @@
-import numpy as np
+import sys
 import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+import numpy as np
 import mrcfile
 import cv2
 import glob
